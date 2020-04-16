89588dcc45a9faa6b0e35d9445a53769eafac91e
Fulvio Todone <fromant65@gmail.com>
2020-04-16 16:10:30 -0300

    Agregada funcion que verifica si hay alguna columna vacia

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -7,4 +7,30 @@ def test_contar_celdas_ocupadas():
 		for celda in fila:
 			contador=contador+celda
 	assert contador==15
+def test_celdas_no_menor_15():
+	mi_carton = carton();
+	contador = 0
+	for fila in mi_carton:
+		for celda in fila:
+			contador=contador+celda
+	assert contador>14
+def test_celdas_no_mayor_15():
+	mi_carton = carton()
+	contador = 0
+	for fila in mi_carton:
+		for celda in fila:
+			contador=contador+celda
+	assert contador<16
 	
+def test_columnas_ocupadas():
+	mi_carton = carton()
+	contador=(0,0,0,0,0,0,0,0,0)
+	for fila in mi_carton:
+		contador=[x + y for x, y in zip(contador, fila)]
+	hay_una_vacia=0
+	for celda in contador:
+		if(celda==0):
+			hay_una_vacia=1
+	assert hay_una_vacia==0
+		
+		
