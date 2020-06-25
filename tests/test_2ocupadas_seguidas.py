c7db8966a26473bb4d7adcb7ccde410f618c8ad9
Fulvio Todone <fromant65@gmail.com>
2020-06-25 11:58:18 -0300

    arreglados los tests en carpeta tests/

--- tests/test_2ocupadas_seguidas.py
+++ tests/test_2ocupadas_seguidas.py
@@ -2,4 +2,4 @@ from src.bingo import t_2ocupadas_seguidas
 from src.bingo import carton1
 
 def test_2ocupadas_seguidas():
-    assert t_2ocupadas_seguidas(carton1())
+    assert t_2ocupadas_seguidas(carton1()) == True
--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -3,7 +3,7 @@ from src.bingo import t_columnas_ocupadas
 from src.bingo import carton2
 
 def test_contar_celdas_ocupadas():
-	assert t_contar_celdas_ocupadas(carton2()) == 12
+	assert t_contar_celdas_ocupadas(carton2()) == True
 
 def test_columnas_ocupadas():
-	assert t_columnas_ocupadas(carton2())==0
+	assert t_columnas_ocupadas(carton2()) == True
